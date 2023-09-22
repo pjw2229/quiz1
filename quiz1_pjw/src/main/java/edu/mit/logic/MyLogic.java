30b2b9230f5adfe5c96175ec64538eb48a5c8c00
pjw <pjw@DESKTOP-HKR7D8L>
2023-09-22 14:16:27 +0900

    MyLogic 클래스 생성. 내용도 작성했다. 아직 annotation은 붙이지 못했다.

---
+++ quiz1_pjw/src/main/java/edu/mit/logic/MyLogic.java
@@ -0,0 +1,18 @@
+package edu.mit.logic;
+
+public class MyLogic {
+	public static int addUp(int num1, int num2) {	// num1 ~ num2까지 전부 더해 return
+		int sum = 0;
+		for(int i = num1; i <= num2; i++)
+			sum += i;
+		return sum;
+	}
+	
+	public static int evenAddUp(int num1, int num2) {	// num1 ~ num2까지 짝수만 더해 return
+		int sum = 0;
+		for(int i = num1; i <= num2; i++)
+			if(i % 2 == 0)
+				sum += i;
+		return sum;
+	}
+}
