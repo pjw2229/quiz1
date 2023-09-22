07f4337ca88288ea8e5ba4e5bc3db924789bf31c
pjw <pjw@DESKTOP-HKR7D8L>
2023-09-22 15:55:08 +0900

    8번 완료. 그런데 이렇게 넘겨주는게 맞는지...

--- quiz1_pjw/src/main/java/edu/mit/controller/QuizController.java
+++ quiz1_pjw/src/main/java/edu/mit/controller/QuizController.java
@@ -3,7 +3,11 @@ package edu.mit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.mit.domain.TwoVO;
 
 @Controller
 @RequestMapping("/quiz/**")
@@ -16,5 +20,15 @@ public class QuizController {
 	@GetMapping("/quiz2")
 	public void gotoQuiz2() {
 		
+	}
+	
+	@PostMapping("/quiz2")
+	public String theQuiz2(int num1, int num2, RedirectAttributes rttr) {
+		TwoVO two = new TwoVO();
+		two.setNum1(num1);
+		two.setNum2(num2);
+		rttr.addFlashAttribute("n1", two.getNum1());
+		rttr.addFlashAttribute("n2", two.getNum2());
+		return "redirect:/quiz/quiz2";
 	}
 }
--- quiz1_pjw/src/main/webapp/WEB-INF/views/quiz/quiz2.jsp
+++ quiz1_pjw/src/main/webapp/WEB-INF/views/quiz/quiz2.jsp
@@ -7,9 +7,12 @@
 </head>
 <body>
 	<form action="quiz2" method="post">
+		num1 : <input type="number" name="num1"><br>
+		num2 : <input type="number" name="num2"><br>
+		<input type="submit" value="넘기기">
 	</form>
-	num1 : <input type="number" name="num1"><br>
-	num2 : <input type="number" name="num2"><br>
-	<input type="submit" value="넘기기">
+	
+	TwoVO의 num1 : ${n1}<br>
+	TwoVO의 num2 : ${n2}<br>
 </body>
 </html>
